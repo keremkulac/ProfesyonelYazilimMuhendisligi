6b769b9f10207e747449c67666c15784c070ff4a
keremkulac <keremkulac14@gmail.com>
2022-12-29 22:41:21 +0300

    Product-view componenti eklendi

--- frontend/app/src/app/components/product-view/product-view.component.ts
+++ frontend/app/src/app/components/product-view/product-view.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product/product.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Product } from 'src/app/models/product.model';
+import { List } from 'src/app/models/list.model';
 
 @Component({
   selector: 'app-product-view',
@@ -7,8 +11,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductViewComponent implements OnInit {
 
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
+  lists: List[] ;
+  products?: Product[] ;
+
+  selectedListId: string;
+
+  constructor(private productService: ProductService, private route: ActivatedRoute, private router: Router) { }
+
+  ngOnInit() {
+    this.route.params.subscribe(
+      (params: Params) => {
+        if (params.listId) {
+          this.selectedListId = params.listId;
+          this.productService.getProducts(params.listId).subscribe((product: any)=> {
+            this.products = product;
+          })
+        } else {
+          this.products = undefined;
+        }
+      }
+    )
+
+
+    this.productService.getLists().subscribe((list : any) => {
+      this.lists = list;
+    })
+    
   }
-  
+
+  onProductClick(product: Product) {
+    // we want to set the task to completed
+    this.productService.complete(product).subscribe(() => {
+      // the task has been set to completed successfully
+      console.log("Completed successully!");
+      product.completed = product.completed;
+    })
+  }
+
+  onDeleteListClick() {
+    this.productService.deleteList(this.selectedListId).subscribe((res: any) => {
+      this.router.navigate(['/lists']);
+      console.log(res);
+    })
+  }
+
+  onDeleteProductClick(id: string) {
+    this.productService.deleteProduct(this.selectedListId, id).subscribe((res: any) => {
+      this.products = this.products!.filter(val => val._id !== id);
+      console.log(res);
+    })
+  }
+
 }
