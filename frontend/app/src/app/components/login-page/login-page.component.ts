e6c8468963d8171e86f1cfc6beb1da9e0f0cfdf2
Huseyin Tekin <hustekin35@gmail.com>
2022-12-29 21:07:30 +0300

    Login page component eklendi

--- frontend/app/src/app/components/login-page/login-page.component.ts
+++ frontend/app/src/app/components/login-page/login-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { HttpResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -7,8 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
+  constructor(private authService: AuthService, private router: Router) { }
+
+  ngOnInit() {
   }
+
+  onLoginButtonClicked(email: string, password: string) {
+    this.authService.login(email, password).subscribe((res: HttpResponse<any>) => {
+      if (res.status === 200) {
+        // we have logged in successfully
+        this.router.navigate(['/lists']);
+      }
+      console.log(res);
+      
+    });
+    
+  }    
 
 }
