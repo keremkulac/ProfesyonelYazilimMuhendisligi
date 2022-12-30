1d9cef93b5790d132857577dea617c4ae4dad450
toncuk <toncuk@gmail.com>
2022-12-30 18:19:44 +0300

    edit-list componenti olusturuldu

--- frontend/app/src/app/components/edit-list/edit-list.component.ts
+++ frontend/app/src/app/components/edit-list/edit-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ProductService } from 'src/app/services/product/product.service';
 
 @Component({
   selector: 'app-edit-list',
@@ -6,7 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-list.component.scss']
 })
 export class EditListComponent implements OnInit {
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
+
+  constructor(private route: ActivatedRoute, private productService: ProductService, private router: Router) { }
+
+  listId: string;
+
+  
+  ngOnInit() {
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.listId = params.listId;
+        console.log(params.listId);
+      }
+    )
   }
+
+  updateList(title: string) {
+    this.productService.updateList(this.listId, title).subscribe(() => {
+      this.router.navigate(['/lists', this.listId]);
+    })
+  }
+
 }
